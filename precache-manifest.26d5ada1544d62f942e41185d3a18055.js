ae15ea7ace95b8bbe52cd21beb41af2486ed1f57
Alanhawk <alalanruiz@gmail.com>
2020-07-24 23:14:07 +0200

    Updates

---
+++ precache-manifest.26d5ada1544d62f942e41185d3a18055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0989eb6c0c10b50942b19fea5446d88b",
+    "url": "/breaking-bad-quotes/index.html"
+  },
+  {
+    "revision": "72aa341d0d495928cbb8",
+    "url": "/breaking-bad-quotes/static/css/main.6a03fd94.chunk.css"
+  },
+  {
+    "revision": "bf7e541b29b38394ba5e",
+    "url": "/breaking-bad-quotes/static/js/2.c85fab59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-quotes/static/js/2.c85fab59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72aa341d0d495928cbb8",
+    "url": "/breaking-bad-quotes/static/js/main.3f7a92e5.chunk.js"
+  },
+  {
+    "revision": "65ac768ab71c316720e7",
+    "url": "/breaking-bad-quotes/static/js/runtime-main.51db5e93.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breaking-bad-quotes/static/media/logo.427a4adc.svg"
+  }
+]);
